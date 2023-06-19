c2a3f129d0af0755f404596c6846ae0b276e05e9
Maxime-Alexandre Gosselin <61528391+Notlein@users.noreply.github.com>
2023-06-19 13:49:41 -0400

    on va l'avoir

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,101 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID SELECT_01 = 386873425U;
+        static const AkUniqueID TEST_ALEAS_01 = 2872867098U;
+        static const AkUniqueID TEST_ALEAS_02 = 2872867097U;
+        static const AkUniqueID TEST_ALEAS_03 = 2872867096U;
+        static const AkUniqueID TRANSITION_INTRO_ALEAS = 3739746622U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace EOLITHES
+        {
+            static const AkUniqueID GROUP = 2060198956U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID EOLITHE_01 = 518197153U;
+                static const AkUniqueID EOLITHE_02 = 518197154U;
+                static const AkUniqueID EOLITHE_03 = 518197155U;
+                static const AkUniqueID EOLITHE_04 = 518197156U;
+                static const AkUniqueID EOLITHE_05 = 518197157U;
+                static const AkUniqueID EOLITHE_06 = 518197158U;
+                static const AkUniqueID EOLITHE_07 = 518197159U;
+                static const AkUniqueID EOLITHE_08 = 518197160U;
+                static const AkUniqueID EOLITHE_09 = 518197161U;
+                static const AkUniqueID EOLITHE_10 = 534974739U;
+                static const AkUniqueID EOLITHE_11 = 534974738U;
+                static const AkUniqueID EOLITHE_12 = 534974737U;
+                static const AkUniqueID EOLITHE_13 = 534974736U;
+                static const AkUniqueID EOLITHE_14 = 534974743U;
+                static const AkUniqueID EOLITHE_15 = 534974742U;
+                static const AkUniqueID EOLITHE_16 = 534974741U;
+                static const AkUniqueID EOLITHE_17 = 534974740U;
+                static const AkUniqueID EOLITHE_18 = 534974747U;
+                static const AkUniqueID EOLITHE_19 = 534974746U;
+                static const AkUniqueID EOLITHE_20 = 551752326U;
+                static const AkUniqueID EOLITHE_21 = 551752327U;
+                static const AkUniqueID EOLITHE_22 = 551752324U;
+                static const AkUniqueID EOLITHE_23 = 551752325U;
+                static const AkUniqueID EOLITHE_24 = 551752322U;
+                static const AkUniqueID EOLITHE_25 = 551752323U;
+                static const AkUniqueID EOLITHE_26 = 551752320U;
+                static const AkUniqueID EOLITHE_27 = 551752321U;
+                static const AkUniqueID EOLITHE_28 = 551752334U;
+                static const AkUniqueID EOLITHE_29 = 551752335U;
+                static const AkUniqueID EOLITHE_30 = 568530041U;
+                static const AkUniqueID EOLITHE_31 = 568530040U;
+                static const AkUniqueID EOLITHE_32 = 568530043U;
+                static const AkUniqueID EOLITHE_33 = 568530042U;
+                static const AkUniqueID EOLITHE_34 = 568530045U;
+                static const AkUniqueID EOLITHE_35 = 568530044U;
+                static const AkUniqueID EOLITHE_36 = 568530047U;
+                static const AkUniqueID EOLITHE_37 = 568530046U;
+                static const AkUniqueID EOLITHE_38 = 568530033U;
+            } // namespace SWITCH
+        } // namespace EOLITHES
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PAN_PAYSAGE = 4014498361U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ALEAS_01 = 1877151555U;
+        static const AkUniqueID TEST_01 = 3020476437U;
+        static const AkUniqueID TEST_02 = 3020476438U;
+        static const AkUniqueID TEST_03 = 3020476439U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
